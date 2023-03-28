e1ad141cddd5dc12fcd01a71cf00e8f59d5d1546
Lautaro Jofré <lautaro.jofre2004@gmail.com>
2023-03-28 00:52:08 -0300

    BackEnd(Cont)

--- BackEnd/mgb/src/main/java/com/portfolio/mgb/Security/Entity/UsuarioPrincipal.java
+++ BackEnd/mgb/src/main/java/com/portfolio/mgb/Security/Entity/UsuarioPrincipal.java
@@ -6,7 +6,6 @@ package com.portfolio.mgb.Security.Entity;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,13 +15,14 @@ import org.springframework.security.core.userdetails.UserDetails;
  *
  * @author User
  */
-public class UsuarioPrincipal implements UserDetails{
+public class UsuarioPrincipal implements UserDetails {
+
     private String nombre;
     private String nombreUsuario;
     private String email;
     private String password;
     private final Collection<? extends GrantedAuthority> authorities;
-    
+
     //Constructor
     public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
         this.nombre = nombre;
@@ -31,47 +31,58 @@ public class UsuarioPrincipal implements UserDetails{
         this.password = password;
         this.authorities = authorities;
     }
-    
-    public static UsuarioPrincipal build(Usuario usuario){
-        List<GrantedAuthority> authorities= (List<GrantedAuthority>) usuario.getRoles().stream()
+
+    public static UsuarioPrincipal build(Usuario usuario) {
+        List<GrantedAuthority> authorities = usuario.getRoles().stream()
                 .map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name()))
                 .collect(Collectors.toList());
-        return new UsuarioPrincipal(usuario.getNombre(),usuario.getNombreUsuario(),usuario.getEmail(),usuario.getPassword(),authorities);
+        return new UsuarioPrincipal(usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(), usuario.getPassword(), authorities);
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return password;
     }
 
-    @Override
-    public String getUsername() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEMail() {
+        return email;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
-    
+
+    @Override
+    public String getUsername() {
+        return nombreUsuario;
+    }
 }
---
+++ BackEnd/mgb/src/main/java/com/portfolio/mgb/Security/Service/UsuarioService.java
@@ -0,0 +1,26 @@
+
+package com.portfolio.mgb.Security.Service;
+
+import com.portfolio.mgb.Security.Entity.Usuario;
+import com.portfolio.mgb.Security.Repository.iUsuarioRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    iUsuarioRepository iusuarioRepository;
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.existByNombreUsuario(nombreUsuario);
+    }
+    public boolean existsByEmail(String email){
+        return iusuarioRepository.existByEMail(email);
+    }
+    
+}
